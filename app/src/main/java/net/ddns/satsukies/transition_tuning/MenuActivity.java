232f7ec816e5376cd28f0e5ee581ee59bf227c6f
satsukies <satsukies@gmail.com>
2017-07-14 01:04:45 +0900

    Create android project

---
+++ app/src/main/java/net/ddns/satsukies/transition_tuning/MenuActivity.java
@@ -0,0 +1,12 @@
+package net.ddns.satsukies.transition_tuning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_menu);
+  }
+}
